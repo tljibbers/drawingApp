0529c6a9d7c0d0a8934e428f2d5a2f0ccb8aadc8
tljibbers <aahijoh@gmail.com>
2026-01-06 22:37:56 -0800

    added prototype color switching

--- main.py
+++ main.py
@@ -6,16 +6,18 @@ import sys
 
 pygame.init()
 
-screen_width = 800
+screen_width = 1200
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Drawing Application")
 
 white = (255,255,255)
 container = []
-
-
+drawing = False
 running = True
+number = ''
+
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -23,18 +25,29 @@ while running:
         
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
-                mousePos = event.pos
-                container.append(mousePos)
+                drawing = True
+                #mousePos = event.pos
+                #container.append(mousePos)
+        elif event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
+                drawing = False
         
-        if event.type == pygame.KEYDOWN:
+        elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_DELETE:
+                print(container)
                 container.clear()
+                screen.fill(colors.BLACK)
+            if event.unicode.isdigit():
+                number = int(event.unicode)
 
-    screen.fill(white)
+    if drawing:
+        mouse_pos = pygame.mouse.get_pos()
+        if(number == ''):
+            test = pygame.draw.rect(screen, colors.BLUE, pygame.Rect(mouse_pos[0], mouse_pos[1], 10, 10))
+        else:
+            test = pygame.draw.rect(screen, colors.colorWheel[number], pygame.Rect(mouse_pos[0], mouse_pos[1], 10, 10))
 
-    for x,y in container:
-        test = shape.Rectangle(5, 5, colors.BLUE)
-        test.draw(screen, x, y)
+
     pygame.display.flip()
 
 pygame.quit()
