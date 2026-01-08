ba6bf10448ca0a1684473231b10f4383b7c3b4e0
tljibbers <aahijoh@gmail.com>
2026-01-07 17:19:54 -0800

    added prototype ctrl z

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ container = []
 drawing = False
 running = True
 number = ''
+history = []
+eraseHistory = False
 
 
 while running:
@@ -26,11 +28,14 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 drawing = True
-                #mousePos = event.pos
-                #container.append(mousePos)
         elif event.type == pygame.MOUSEBUTTONUP:
             if event.button == 1:
                 drawing = False
+                container.append(history)
+                last_segment = container.pop()
+                #print(container)
+                print(last_segment)
+
         
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_DELETE:
@@ -39,13 +44,21 @@ while running:
                 screen.fill(colors.BLACK)
             if event.unicode.isdigit():
                 number = int(event.unicode)
+            if event.key == pygame.K_z and pygame.key.get_mods() and pygame.KMOD_CTRL:
+                eraseHistory = True
 
     if drawing:
         mouse_pos = pygame.mouse.get_pos()
         if(number == ''):
             test = pygame.draw.rect(screen, colors.BLUE, pygame.Rect(mouse_pos[0], mouse_pos[1], 10, 10))
+            history.append(mouse_pos)
         else:
             test = pygame.draw.rect(screen, colors.colorWheel[number], pygame.Rect(mouse_pos[0], mouse_pos[1], 10, 10))
+
+    if eraseHistory:
+        for x, y in last_segment:
+            pygame.draw.rect(screen, colors.BLACK, pygame.Rect(x, y, 10, 10))
+        eraseHistory = False
 
 
     pygame.display.flip()
